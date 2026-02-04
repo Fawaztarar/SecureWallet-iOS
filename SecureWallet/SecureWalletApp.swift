c1f79d20aae7ea073446ac144462c14ed7dfcaa4
Fawaz Tarar <fawaztarar@gmail.com>
2026-02-04 18:04:00 +0000

    xcode project

---
+++ SecureWallet/SecureWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureWalletApp.swift
+//  SecureWallet
+//
+//  Created by Fawaz Tarar on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SecureWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
